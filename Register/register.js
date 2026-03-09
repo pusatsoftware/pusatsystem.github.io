5ebdcf0c6e8b954b5ada72f2d43196ad5f890538
Pusat Software <emirhanpusat0@gmail.com>
2026-03-10 01:59:15 +0300

    Add files via upload

---
+++ Register/register.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('registerForm');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+        // Firestore'a kaydet
+        db.collection("users").doc(userCredential.user.uid).set({
+            name: name,
+            email: email
+        });
+
+        // Başarılı mesaj ve animasyon
+        message.textContent = "Kayıt Başarılı! Yönlendiriliyorsunuz...";
+        message.style.color = "green";
+        message.classList.add("show");
+
+        // 2 saniye sonra login sayfasına yönlendir
+        setTimeout(() => {
+            window.location.href = "../login/login.html";
+        }, 2000);
+    })
+    .catch(err => {
+        message.textContent = err.message;
+        message.style.color = "red";
+        message.classList.add("show");
+    });
+});
