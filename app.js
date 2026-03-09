a44053c38b4ca49501a734e9094b8aab50548c1b
Pusat Software <emirhanpusat0@gmail.com>
2026-03-10 01:30:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Firebase SDK script ile uyumlu (compat versiyon)
+const firebaseConfig = {
+  apiKey: "AIzaSyB9WxNwFwGS1OCFXNo00cP4XfKnsq01k-E",
+  authDomain: "pusatsoftware-1520d.firebaseapp.com",
+  projectId: "pusatsoftware-1520d",
+  storageBucket: "pusatsoftware-1520d.firebasestorage.app",
+  messagingSenderId: "9573540669",
+  appId: "1:9573540669:web:4c6bce642cb28e067d5b54",
+  measurementId: "G-PEMB6TKB7F"
+};
+
+// Firebase başlat
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
