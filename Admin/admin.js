72d46a858f6d21efd5ded55d9d5d88c63a65a74b
Pusat Software <emirhanpusat0@gmail.com>
2026-03-10 15:21:39 +0300

    Update admin.js

--- Admin/admin.js
+++ Admin/admin.js
@@ -6,14 +6,13 @@ function startApp() {
         const rows = document.getElementById('userDataRows');
         rows.innerHTML = "";
         let sayac = 1; 
-
         snapshot.forEach((doc) => {
             const u = doc.data();
             rows.innerHTML += `
                 <div class="user-row">
                     <span style="opacity:0.4; font-weight:bold; color:var(--primary);">${sayac}</span>
                     <span style="font-weight:600">${u.name}</span>
-                    <span style="color:#94a3b8; font-size: 13px;">${u.email}</span>
+                    <span style="color:#text-muted; font-size: 13px;">${u.email}</span>
                     <span style="color:#a855f7; font-family:monospace;">${u.password}</span>
                     <div style="display:flex; gap:15px; justify-content: flex-end;">
                         <button onclick="openEdit('${doc.id}','${u.name}','${u.email}','${u.password}')" style="background:none; border:none; cursor:pointer; font-size:18px;">✏️</button>
@@ -25,6 +24,7 @@ function startApp() {
     });
 }
 
+// DÜZENLEME
 function openEdit(id, n, e, p) {
     currentEditId = id;
     document.getElementById('editName').value = n;
@@ -47,6 +47,7 @@ function updateUser() {
     });
 }
 
+// SİLME
 function deleteUser(id) {
     userToDelete = id; 
     document.getElementById('deleteModalOverlay').style.display = 'flex';
@@ -55,15 +56,17 @@ function closeDeleteModal() { document.getElementById('deleteModalOverlay').styl
 
 function confirmDelete() {
     if(userToDelete) {
+        closeDeleteModal(); // Onay kutusunu hemen kapat
         db.collection("users").doc(userToDelete).delete().then(() => {
-            closeDeleteModal(); 
             const delSuccess = document.getElementById('deleteSuccessModalOverlay');
-            delSuccess.style.display = 'flex';
+            delSuccess.style.display = 'flex'; // Başarı mesajını patlat
+            userToDelete = "";
             setTimeout(() => { delSuccess.style.display = 'none'; }, 2000);
         });
     }
 }
 
+// ÇIKIŞ
 document.getElementById('logout').addEventListener('click', () => {
     auth.signOut().then(() => { window.location.href = "../index.html"; });
 });
